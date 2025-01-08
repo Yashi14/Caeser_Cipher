b9258e8000e90aaceeee8d02d5b79fc570ca719a
Yashi14 <yashinimje142004@gmail.com>
2025-01-08 23:21:38 +0530

    New folder

---
+++ Secret_Auction/auction.py
@@ -0,0 +1,20 @@
+import art
+print(art.logo)
+
+bidders = {}
+
+
+still_bidding = False
+while still_bidding:
+    bidder_name=input("Enter Your Name:")
+    bidder_price=input("Enter Your bid:")
+    bidders[bidder_name]=bidder_price
+    
+
+
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+
