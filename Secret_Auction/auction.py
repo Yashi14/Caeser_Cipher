28dfcf283e0db62024fca62da768315a75c53227
Yashi14 <yashinimje142004@gmail.com>
2025-01-08 23:49:42 +0530

    Modifications in auction.py

--- Secret_Auction/auction.py
+++ Secret_Auction/auction.py
@@ -1,20 +1,33 @@
 import art
+
 print(art.logo)
 
+
+def highest_bid(bid_money):
+    highest_money = 0
+    winner = ""
+    for people in bid_money:
+        bid_amount = bid_money[people]
+        if bid_amount > highest_money:
+            highest_money = bid_amount
+            winner = people
+
+    print(f"The winner is {winner} with total bid of {highest_money} ")
+
+
 bidders = {}
-
-
-still_bidding = False
+still_bidding = True
 while still_bidding:
-    bidder_name=input("Enter Your Name:")
-    bidder_price=input("Enter Your bid:")
-    bidders[bidder_name]=bidder_price
-    
+    bidder_name = input("Enter Your Name:")
+    bidder_price = int(input("Enter Your bid:"))
+    bidders[bidder_name] = bidder_price
+    should_continue = input("Are there more bidders?")
+    if should_continue == "yes":
+        print("\n" * 30)
 
+    elif should_continue == "no":
+        highest_bid(bid_money=bidders)
+        still_bidding = False
 
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
-
-
+    else:
+        print("Type yes or no")
