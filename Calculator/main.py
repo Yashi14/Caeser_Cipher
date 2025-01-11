e3da4d4d90ec0a8a4b572eb035e5b30c729004a6
Yashi14 <yashinimje142004@gmail.com>
2025-01-11 23:23:06 +0530

    Changes in Calculator

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,25 +1,53 @@
 import art
-print(art.logo)
+
 
 def add(n1, n2):
     return n1 + n2
 
-def sub(n1, n2):
+
+def subtract(n1, n2):
     return n1 - n2
 
-def mul(n1, n2):
+
+def multiply(n1, n2):
     return n1 * n2
 
-def div(n1, n2):
+
+def divide(n1, n2):
     return n1 / n2
+
 
 operations = {
     "+": add,
-    "-": sub,
-    "*": mul,
-    "/": div,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
 }
 
-first_number = int(input("What is the first number"))
-print("+\n-\n*\n/")
-operation = input("pick an operation")
+# print(operations["*"](4, 8))
+
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
