f8869a7d7df368a5f47c2d985b93a6d67dcc953b
Yashi14 <yashinimje142004@gmail.com>
2025-01-11 23:46:36 +0530

    Changes in calculator project

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,5 +1,5 @@
 import art
-print(art.logo)
+
 
 def add(n1, n2):
     return n1 + n2
@@ -21,26 +21,28 @@ operations = {
 }
 
 def calculator():
+    print(art.logo)
     first_number = int(input("What is the first number:"))
-    print("+\n-\n*\n/")
-    operation = input("pick an operation:")
 
     still_solving = True
     while still_solving:
+        print("+\n-\n*\n/")
+        operation = input("pick an operation:")
         second_number = int(input("What is the next number: "))
-        result = operations[operation](n1=first_number, n2=second_number)
-        print(result)
+        result = operations[operation](first_number, second_number)
+        print(f"{first_number} {operation} {second_number} = {result}")
 
         calc_continue = input(
             f"Type 'y' to continue calculating with {result} ,or type 'n' to start a new calculation: ")
         if calc_continue == "y":
             first_number = result
 
-        if calc_continue == "n":
+        elif calc_continue == "n":
             still_solving = False
             print("\n" * 30)
             calculator()
         else:
             print("Invalid")
+            still_solving= False
 
 calculator()
